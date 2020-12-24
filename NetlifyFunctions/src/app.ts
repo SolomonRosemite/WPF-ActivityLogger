1a3e633252efe984d5ce2d09c29a5fb22f5ab918
Solomon Rosemite™ <60587271+SolomonRosemite@users.noreply.github.com>
2020-12-24 22:26:42 +0100

    More Tests

--- NetlifyFunctions/src/app.ts
+++ NetlifyFunctions/src/app.ts
@@ -1,7 +1,7 @@
 const serverless = require("serverless-http");
 require("dotenv").config();
-//
-// import { createNewUser, IUser, randomString, uuidExists } from './common';
+
+import { createNewUser, IUser, randomString, uuidExists } from './common';
 import * as bodyParser from "body-parser";
 import * as express from "express";
 //
@@ -22,9 +22,9 @@ val.fullError = JSON.stringify(err, ["message", "arguments", "type", "name"])
 }
 
 
-// const auth = admin.auth();
-// const firestore = admin.firestore();
-// const storage = admin.storage();
+const auth = admin.auth();
+const firestore = admin.firestore();
+const storage = admin.storage();
 //
 const appname = "app";
 
@@ -40,77 +40,77 @@ router.get("/", (req, res) => {
      message: val
    });
 });
-//
-// router.post("/auth", async (req, res) => {
-//   const { secret } = req.body;
-//
-//   if(secret === undefined || secret === null) {
-//     const id = await createUUID(randomString(20));
-//     const user = createNewUser(id);
-//
-//     const result = await signUpUser(user).catch((err) => {
-//       console.log(err)
-//       // TODO: Report Error
-//     })
-//
-//     if (!result) {
-//       // The Email either already in use or something else went wrong
-//       res.status(500).json({
-//         message: "Something when wrong. Try to regenerate a new secret"
-//       })
-//       return;
-//     }
-//
-//     await firestore.doc("/users/" + result.uid).set({
-//       action: "waiting"
-//     })
-//
-//     await firestore.doc("/secrets/" + result.uid).set({
-//       secret: user.uuid,
-//       email: user.email,
-//       password: user.password,
-//       uid: result.uid,
-//     })
-//
-//     res.json({
-//       user: user
-//     });
-//     return;
-//   }
-//
-//   const user = await uuidExists(secret, firestore)
-//
-//   if (!user) {
-//     res.json({
-//       error: "User with this Secret does not exist."
-//     });
-//     return;
-//   }
-//
-//
-//   res.json({
-//     user: {
-//       email: user.email,
-//       password: user.password
-//     }
-//   });
-// })
-//
-// async function createUUID(id: string): Promise<string> {
-//   const res = await uuidExists(id, firestore)
-//   if (res) {
-//     return await createUUID(randomString(20));
-//   }
-//
-//   return id;
-// }
-//
-// function signUpUser(user: IUser) {
-//   return auth.createUser({
-//     email: user.email,
-//     password: user.password
-//   })
-// }
-//
+
+router.post("/auth", async (req, res) => {
+  const { secret } = req.body;
+
+  if(secret === undefined || secret === null) {
+    const id = await createUUID(randomString(20));
+    const user = createNewUser(id);
+
+    const result = await signUpUser(user).catch((err) => {
+      console.log(err)
+      // TODO: Report Error
+    })
+
+    if (!result) {
+      // The Email either already in use or something else went wrong
+      res.status(500).json({
+        message: "Something when wrong. Try to regenerate a new secret"
+      })
+      return;
+    }
+
+    await firestore.doc("/users/" + result.uid).set({
+      action: "waiting"
+    })
+
+    await firestore.doc("/secrets/" + result.uid).set({
+      secret: user.uuid,
+      email: user.email,
+      password: user.password,
+      uid: result.uid,
+    })
+
+    res.json({
+      user: user
+    });
+    return;
+  }
+
+  const user = await uuidExists(secret, firestore)
+
+  if (!user) {
+    res.json({
+      error: "User with this Secret does not exist."
+    });
+    return;
+  }
+
+
+  res.json({
+    user: {
+      email: user.email,
+      password: user.password
+    }
+  });
+})
+
+async function createUUID(id: string): Promise<string> {
+  const res = await uuidExists(id, firestore)
+  if (res) {
+    return await createUUID(randomString(20));
+  }
+
+  return id;
+}
+
+function signUpUser(user: IUser) {
+  return auth.createUser({
+    email: user.email,
+    password: user.password
+  })
+}
+
 module.exports.handler = serverless(app);
 export { app };
--- NetlifyFunctions/src/common.ts
+++ NetlifyFunctions/src/common.ts
@@ -1,46 +1,46 @@
-// interface IUser {
-//   uuid: string;
-//   email: string;
-//   password: string;
-// }
-//
-// async function uuidExists(uuid: string, firestore: FirebaseFirestore.Firestore): Promise<IUser | undefined> {
-//   const results = await firestore.collection("/secrets")
-//                     .where("secret", "==", uuid)
-//                     .get()
-//                     .catch((err) => {
-//                       console.log(err)
-//                     });
-//
-//
-//   if (results && !results.empty) {
-//     return results.docs[0].data() as IUser;
-//   }
-//
-//   return undefined;
-// }
-//
-// function createNewUser(id: string): IUser {
-//   const email = `${id}@${randomString(10)}.com`
-//   const password = randomString(20);
-//
-//   return {
-//     email: email,
-//     password: password,
-//     uuid: id
-//   }
-// }
-//
-// function randomString(length: number) {
-//    let result = '';
-//    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-//    const charactersLength = characters.length;
-//
-//    for ( var i = 0; i < length; i++ ) {
-//       result += characters.charAt(Math.floor(Math.random() * charactersLength));
-//    }
-//
-//    return result;
-// }
-//
-// export { IUser, createNewUser, uuidExists, randomString }
+interface IUser {
+  uuid: string;
+  email: string;
+  password: string;
+}
+
+async function uuidExists(uuid: string, firestore: FirebaseFirestore.Firestore): Promise<IUser | undefined> {
+  const results = await firestore.collection("/secrets")
+                    .where("secret", "==", uuid)
+                    .get()
+                    .catch((err) => {
+                      console.log(err)
+                    });
+
+
+  if (results && !results.empty) {
+    return results.docs[0].data() as IUser;
+  }
+
+  return undefined;
+}
+
+function createNewUser(id: string): IUser {
+  const email = `${id}@${randomString(10)}.com`
+  const password = randomString(20);
+
+  return {
+    email: email,
+    password: password,
+    uuid: id
+  }
+}
+
+function randomString(length: number) {
+   let result = '';
+   const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+   const charactersLength = characters.length;
+
+   for ( var i = 0; i < length; i++ ) {
+      result += characters.charAt(Math.floor(Math.random() * charactersLength));
+   }
+
+   return result;
+}
+
+export { IUser, createNewUser, uuidExists, randomString }
