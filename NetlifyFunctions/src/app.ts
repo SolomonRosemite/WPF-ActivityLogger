64fe2a22ab1deed00c9cb300df167c5a013af57e
Solomon Rosemite™ <60587271+SolomonRosemite@users.noreply.github.com>
2020-12-24 17:41:52 +0100

    🙋‍♀️User now get Created by Server Functions

--- NetlifyFunctions/src/app.ts
+++ NetlifyFunctions/src/app.ts
@@ -1,8 +1,22 @@
 const serverless = require("serverless-http");
 
+import { createNewUser, IUser, randomString, uuidExists } from './common';
 import * as bodyParser from "body-parser";
 import * as express from "express";
-import { createNewUser, IUser, randomString, uuidExists } from './common';
+
+import { firebaseConfig } from "./credentials";
+import firebase from "firebase/app";
+
+// Firebase services
+import "firebase/auth";
+import "firebase/firestore";
+import "firebase/storage";
+
+const project = firebase.initializeApp(firebaseConfig);
+
+const auth = project.auth();
+const firestore = project.firestore();
+const storage = project.storage();
 
 const appname = "app";
 
@@ -15,10 +29,12 @@ app.use(bodyParser.json());
 app.use(`/.netlify/functions/${appname}`, router);
 
 router.get("/", (req, res) => {
-  res.json({ message: "Hi" });
+  res.json({
+     message: "Hi"
+   });
 });
 
-router.post("/auth", (req, res) => {
+router.post("/auth", async (req, res) => {
   const { secret } = req.body;
 
   if(secret === undefined || secret === null) {
@@ -26,6 +42,22 @@ router.post("/auth", (req, res) => {
     const user = createNewUser(id);
     users.push(user);
 
+    const result = await signUpUser(user).catch((err) => {
+      console.log(err)
+      // TODO: Report Error
+    })
+
+    if (!result || !result.user) {
+      // The Email either already in use or something else went wrong
+      res.status(500).json({
+        message: "Something when wrong. Try to regenerate a new secret"
+      })
+      return;
+    }
+
+    await firestore.doc("/users/" + result.user.uid).set({
+      action: "waiting"
+    })
 
     res.json({
       user: user
@@ -42,17 +74,22 @@ router.post("/auth", (req, res) => {
     return;
   }
 
-  res.json({user: user});
+  res.json({
+    user: user
+  });
 })
 
 function createUUID(id: string, list: IUser[]): string {
   const res = uuidExists(id, list)
-
   if (res) {
     return createUUID(randomString(20), list);
   }
 
   return id;
+}
+
+function signUpUser(user: IUser) {
+  return auth.createUserWithEmailAndPassword(user.email, user.password);
 }
 
 module.exports.handler = serverless(app);
