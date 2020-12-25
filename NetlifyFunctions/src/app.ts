086d37ea12c852097a8d978393368175c20156a3
Solomon Rosemite™ <60587271+SolomonRosemite@users.noreply.github.com>
2020-12-25 17:25:01 +0100

    🧰Fixed Issue with Netlify Functions & Firebase [ Merged test into master branch  ]

--- NetlifyFunctions/src/app.ts
+++ NetlifyFunctions/src/app.ts
@@ -1,16 +1,16 @@
 const serverless = require("serverless-http");
+require("dotenv").config();
 
-import { createNewUser, IUser, randomString, uuidExists } from './common';
+import { createNewUser, IUser, randomString, uuidExists } from "./common";
 import * as bodyParser from "body-parser";
 import * as express from "express";
-
-const serviceAccount = require("../serviceAccount.json");
-
-import * as admin from 'firebase-admin';
+//
+const serviceAccount = JSON.parse(process.env.serviceAccount as any);
+import * as admin from "firebase-admin";
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
-  storageBucket: "rosemite-activities.appspot.com"
+  storageBucket: "rosemite-activities.appspot.com",
 });
 
 const auth = admin.auth();
@@ -22,73 +22,71 @@ const appname = "app";
 const app = express();
 const router = express.Router();
 
-
 app.use(bodyParser.json());
 app.use(`/.netlify/functions/${appname}`, router);
 
 router.get("/", (req, res) => {
   res.json({
-     message: "Hi"
-   });
+    message: "Hey",
+  });
 });
 
 router.post("/auth", async (req, res) => {
   const { secret } = req.body;
 
-  if(secret === undefined || secret === null) {
+  if (secret === undefined || secret === null) {
     const id = await createUUID(randomString(20));
     const user = createNewUser(id);
 
     const result = await signUpUser(user).catch((err) => {
-      console.log(err)
+      console.log(err);
       // TODO: Report Error
-    })
+    });
 
     if (!result) {
       // The Email either already in use or something else went wrong
       res.status(500).json({
-        message: "Something when wrong. Try to regenerate a new secret"
-      })
+        message: "Something when wrong. Try to regenerate a new secret",
+      });
       return;
     }
 
     await firestore.doc("/users/" + result.uid).set({
-      action: "waiting"
-    })
+      action: "waiting",
+    });
 
     await firestore.doc("/secrets/" + result.uid).set({
       secret: user.uuid,
       email: user.email,
       password: user.password,
       uid: result.uid,
-    })
+    });
 
     res.json({
-      user: user
+      user: user,
     });
     return;
   }
 
-  const user = await uuidExists(secret, firestore)
+  const user = await uuidExists(secret, firestore);
 
   if (!user) {
     res.json({
-      error: "User with this Secret does not exist."
+      error: "User with this Secret does not exist.",
     });
     return;
   }
-
 
   res.json({
     user: {
       email: user.email,
-      password: user.password
-    }
+      password: user.password,
+    },
   });
-})
+});
 
 async function createUUID(id: string): Promise<string> {
-  const res = await uuidExists(id, firestore)
+  const res = await uuidExists(id, firestore);
   if (res) {
     return await createUUID(randomString(20));
   }
@@ -99,8 +97,8 @@ async function createUUID(id: string): Promise<string> {
 function signUpUser(user: IUser) {
   return auth.createUser({
     email: user.email,
-    password: user.password
-  })
+    password: user.password,
+  });
 }
 
 module.exports.handler = serverless(app);
